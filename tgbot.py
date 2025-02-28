ea28eebc2767de0bf7eb4762dfa14ce40dad2b02
moka-co <54906285+moka-co@users.noreply.github.com>
2025-02-28 14:47:09 +0100

    Created simple boilerplate telegram bot

---
+++ tgbot.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    token=None
+    if (len(sys.argv)==1):
+        print("Error token required")
+        quit()
+    
+    token=sys.argv[1]
+
+    application = ApplicationBuilder().token(token).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
